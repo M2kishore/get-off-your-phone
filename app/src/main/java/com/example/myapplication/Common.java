df2215c63fb969c79f01e0ec99258f535605cfc4
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-04-30 18:20:05 +0530

    Common stores the percentage screen blocked

--- app/src/main/java/com/example/myapplication/Common.java
+++ app/src/main/java/com/example/myapplication/Common.java
@@ -1,14 +1,6 @@
 package com.example.myapplication;
 
 public class Common {
-    // The EditText String will be
-    // stored in this variable
-    // when MINIMIZE or MAXIMIZE
-    // button is pressed
-    public static String currentDesc = "";
-
-    // The EditText String will be
-    // stored in this variable
-    // when SAVE button is pressed
-    public static String savedDesc = "";
+    //denotes the current percentage
+    public static float percentage_size_decrease = (float) 0.10;
 }
